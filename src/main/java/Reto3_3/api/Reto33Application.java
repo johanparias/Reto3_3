27b7bcc6705744d0ce3bcafff4add47f9e9e1ef2
johanparias <johan.parias.mt@correo.usa.edu.co>
2021-10-20 23:14:16 -0500

    proyecto sprint reto 3

---
+++ src/main/java/Reto3_3/api/Reto33Application.java
@@ -0,0 +1,13 @@
+package Reto3_3.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto33Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto33Application.class, args);
+	}
+
+}
---
+++ src/test/java/Reto3_3/api/Reto33ApplicationTests.java
@@ -0,0 +1,13 @@
+package Reto3_3.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reto33ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
